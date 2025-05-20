cb8ed99cbe8c1bb939d07c695e89aeafe09f4be1
Thieu Nguyen <nguyenthieu2102@gmail.com>
2025-05-20 23:59:47 +0700

    Add examples for GdWnnClassifier and GdWnnRegressor classes

---
+++ examples/exam_GdWnnClassifier_binary.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 22:51, 20/05/2025 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+from sklearn.datasets import load_breast_cancer
+from waveletml import Data, GdWnnClassifier, EarlyStoppingCallback, ModelCheckpointCallback, TensorBoardLoggerCallback
+
+
+## Load data object
+X, y = load_breast_cancer(return_X_y=True)
+data = Data(X, y)
+
+## Split train and test
+data.split_train_test(test_size=0.2, random_state=2, inplace=True, shuffle=True)
+print(data.X_train.shape, data.X_test.shape)
+
+## Scaling dataset
+data.X_train, scaler_X = data.scale(data.X_train, scaling_methods=("standard", "minmax"))
+data.X_test = scaler_X.transform(data.X_test)
+
+data.y_train, scaler_y = data.encode_label(data.y_train)
+data.y_test = scaler_y.transform(data.y_test)
+
+print(type(data.X_train), type(data.y_train))
+
+## Create model
+callbacks = [
+        EarlyStoppingCallback(patience=5, monitor="val_loss"),
+        ModelCheckpointCallback(monitor="val_loss"),
+        TensorBoardLoggerCallback()
+]
+model = GdWnnClassifier(size_hidden=10, wavelet_fn="morlet", act_output=None,
+                        epochs=50, batch_size=16, optim="Adam", optim_paras=None,
+                        valid_rate=0.1, seed=42, verbose=True, callbacks=callbacks)
+## Train the model
+model.fit(X=data.X_train, y=data.y_train)
+
+## Test the model
+y_pred = model.predict(data.X_test)
+print(y_pred)
+print(model.predict_proba(data.X_test))
+
+## Calculate some metrics
+print(model.evaluate(y_true=data.y_test, y_pred=y_pred, list_metrics=["F2S", "CKS", "FBS", "PS", "RS", "NPV", "F1S"]))
