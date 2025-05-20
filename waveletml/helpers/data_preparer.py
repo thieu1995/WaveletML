07745bea1453588029c0598ee2b853e440939616
Thieu Nguyen <nguyenthieu2102@gmail.com>
2025-05-20 23:52:02 +0700

    Fix bug data_preparer module

--- waveletml/helpers/data_preparer.py
+++ waveletml/helpers/data_preparer.py
@@ -6,7 +6,7 @@
 
 import numpy as np
 from sklearn.model_selection import train_test_split
-from unilvq.helpers.scaler import *
+from waveletml.helpers.data_scaler import *
 
 
 class TimeSeriesDifferencer:
