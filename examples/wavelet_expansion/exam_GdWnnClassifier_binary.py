b2e89ba676d7a11994dadefdaa97a2eda869d525
anh9895 <honganh090895@gmail.com>
2025-05-23 16:40:24 +0700

    Update examples for Wavelet Expansion Net

--- examples/wavelet_expansion/exam_GdWnnClassifier_binary.py
+++ examples/wavelet_expansion/exam_GdWnnClassifier_binary.py
@@ -27,7 +27,7 @@ print(type(data.X_train), type(data.y_train))
 
 ## Create model
 model = GdWnnClassifier(size_hidden=10, wavelet_fn="morlet", act_output=None,
-                        epochs=50, batch_size=16, optim="Adam", optim_paras=None,
+                        epochs=50, batch_size=16, optim="Adam", optim_params=None,
                         valid_rate=0.1, seed=42, verbose=True, device=None, wnn_type=CustomWaveletExpansionNetwork)
 ## Train the model
 model.fit(X=data.X_train, y=data.y_train)
---
+++ examples/wavelet_expansion/exam_GdWnnClassifier_sklearn.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 16:18, 23/05/2025 ----------%
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import cross_val_score, train_test_split, GridSearchCV
+from sklearn.pipeline import Pipeline
+from waveletml import DataTransformer, GdWnnClassifier, CustomWaveletExpansionNetwork
+
+
+def get_cross_val_score(X, y, cv=3):
+    ## Train and test
+    model = GdWnnClassifier(size_hidden=10, wavelet_fn="morlet", act_output=None,
+                        epochs=50, batch_size=16, optim="Adam", optim_params=None,
+                        valid_rate=0.1, seed=42, verbose=True, device=None, wnn_type=CustomWaveletExpansionNetwork)
+    return cross_val_score(model, X, y, cv=cv)
+
+
+def get_pipe_line(X, y):
+    ## Split train and test
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=2)
+
+    ## Train and test
+    model = GdWnnClassifier(size_hidden=10, wavelet_fn="morlet", act_output=None,
+                        epochs=50, batch_size=16, optim="Adam", optim_params=None,
+                        valid_rate=0.1, seed=42, verbose=True, device=None, wnn_type=CustomWaveletExpansionNetwork)
+
+    pipe = Pipeline([
+        ("dt", DataTransformer(scaling_methods=("standard", "minmax"))),
+        ("pnn", model)
+    ])
+
+    pipe.fit(X_train, y_train)
+    y_pred = pipe.predict(X_test)
+
+    return model.evaluate(y_true=y_test, y_pred=y_pred, list_metrics=["F2S", "CKS", "FBS", "AS", "RS", "PS"])
+
+
+def get_grid_search(X, y):
+    ## Split train and test
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=2)
+
+    para_grid = {
+        'wavelet_fn': ("morlet", "mexican_hat", "haar"),
+        'size_hidden': [10, 20, 30]
+    }
+
+    ## Create a gridsearch
+    model = GdWnnClassifier(epochs=50, valid_rate=0.1, seed=42, verbose=True, wnn_type=CustomWaveletExpansionNetwork)
+    clf = GridSearchCV(model, para_grid, cv=3, scoring='accuracy', verbose=2)
+    clf.fit(X_train, y_train)
+    print("Best parameters found: ", clf.best_params_)
+    print("Best model: ", clf.best_estimator_)
+    print("Best training score: ", clf.best_score_)
+    print(clf)
+
+    ## Predict
+    y_pred = clf.predict(X_test)
+    return model.evaluate(y_true=y_test, y_pred=y_pred, list_metrics=["F2S", "CKS", "FBS", "AS", "RS", "PS"])
+
+
+## Load data object
+X, y = load_breast_cancer(return_X_y=True)
+
+print(get_cross_val_score(X, y, cv=3))
+print(get_pipe_line(X, y))
+print(get_grid_search(X, y))
