fdbbc254abdb44d953778c9cca3fedf6798c480a
anh9895 <honganh090895@gmail.com>
2025-05-23 16:04:26 +0700

    Update examples for Wavelet feature-based Expansion Neural Network

---
+++ examples/wavelet_expansion/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 16:00, 23/05/2025 ----------%
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
