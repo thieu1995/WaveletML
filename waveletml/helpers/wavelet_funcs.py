5eb3e04447ed17a978ad15875cb1bc5025c6bed9
Thieu Nguyen <nguyenthieu2102@gmail.com>
2025-05-25 12:13:11 +0700

    Add docstring for wavelet_funcs module

--- waveletml/helpers/wavelet_funcs.py
+++ waveletml/helpers/wavelet_funcs.py
@@ -9,12 +9,39 @@ import torch.nn.functional as F
 
 
 def morlet(x):
+    """
+    Computes the Morlet wavelet function.
+
+    Parameters:
+        x (torch.Tensor): Input tensor.
+
+    Returns:
+        torch.Tensor: Output tensor after applying the Morlet wavelet function.
+    """
     return torch.cos(1.75 * x) * torch.exp(-0.5 * x**2)
 
 
 def mexican_hat(x):
+    """
+    Computes the Mexican Hat wavelet function.
+
+    Parameters:
+        x (torch.Tensor): Input tensor.
+
+    Returns:
+        torch.Tensor: Output tensor after applying the Mexican Hat wavelet function.
+    """
     return (1 - x**2) * torch.exp(-0.5 * x**2)
 
 
 def haar(x):
+    """
+    Computes the Haar wavelet function.
+
+    Parameters:
+        x (torch.Tensor): Input tensor.
+
+    Returns:
+        torch.Tensor: Output tensor after applying the Haar wavelet function.
+    """
     return torch.where((x >= -0.5) & (x < 0.5), torch.ones_like(x), torch.zeros_like(x))
